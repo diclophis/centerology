0519ff284661d444d555d0a73f4f82ea640c7259
root <diclophis@gmail.com>
2008-09-22 08:09:24 +0000

    latest image from person falls apart when there is only one person

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,6 +4,7 @@ class Person < ActiveRecord::Base
   has_many_friends
   has_many :findings
   has_many :images, :through => :findings
+  has_many :last_three_images, :source => :image, :through => :findings, :limit => 3, :order => 'created_at DESC'
   validates_presence_of :identity_url
   validates_as_uri :identity_url
   validates_presence_of :email
